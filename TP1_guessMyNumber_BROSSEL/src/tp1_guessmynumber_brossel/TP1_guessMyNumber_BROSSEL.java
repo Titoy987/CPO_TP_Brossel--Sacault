891573936e313c5dc087d262028e438d2f3f95bc
steven <steven@10.3.61.29>
2025-10-22 16:48:19 +0200

    TP1_guessmynumber

---
+++ TP1_guessMyNumber_BROSSEL/src/tp1_guessmynumber_brossel/TP1_guessMyNumber_BROSSEL.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp1_guessmynumber_brossel;
+
+import java.util.Random;
+
+/**
+ *
+ * @author steven
+ */
+public class TP1_guessMyNumber_BROSSEL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Random generateurAleat = new Random();
+        
+        int n = generateurAleat.nextInt(100);
+        System.out.println(n);
+    }
+    
+}
