3c51622fbd26dea0d317bdbf0ba50967b959ccdb
steven <steven@10.3.61.29>
2025-10-22 16:43:23 +0200

    TP1_manipNombre

--- TP1_convertisseur_BROSSEL/src/tp1_convertisseur_brossel/TP1_convertisseur_BROSSEL.java
+++ TP1_convertisseur_BROSSEL/src/tp1_convertisseur_brossel/TP1_convertisseur_BROSSEL.java
@@ -28,6 +28,14 @@ public class TP1_convertisseur_BROSSEL {
     public static double CelciusVersFarenheit(double tCelcius) {
         return (tCelcius * 9 / 5) + 32;
     }
+    public static double KelvinVersFarenheit(double tKelvin) {
+        double tempC = KelvinVersCelcius(tKelvin);
+        return CelciusVersFarenheit(tempC);
+    }
+    public static double FarenheitVersKelvin(double tFarenheit) {
+        double tempC = FarenheitVersCelcius(tFarenheit);
+        return CelciusVersKelvin(tempC);
+    }
      /**
      * @param args the command line arguments
      */
@@ -40,7 +48,48 @@ public class TP1_convertisseur_BROSSEL {
         System.out.println("1) de celcius vers kelvin");
         System.out.println("2) de kelvin vers celcius");
         System.out.println("3) de farenheit vers celcius");
-        System.out.println("4) de ");
+        System.out.println("4) de celcius vers farenheit");
+        System.out.println("5) de Kelvin vers farenheit");
+        System.out.println("6) de farenheit vers kelvin");
+        int choix = Sc.nextInt();
+        double resultat=0;
+        String avant ="";
+        String apres = "";
+        if(choix == 1){
+            resultat = CelciusVersKelvin(var1);
+            avant = "Celcius";
+            apres = "Kelvin";
+        }
+        else if(choix==2){
+            resultat = KelvinVersCelcius(var1);
+            avant = "Kelvin";
+            apres = "Celcius";
+        }
+        else if(choix==3){
+            resultat = FarenheitVersCelcius(var1);
+            avant = "Farenheit";
+            apres = "Celcius";
+        }
+        else if(choix==4){
+            resultat = CelciusVersFarenheit(var1);
+            avant = "Celcius";
+            apres = "Farenheit";
+        }
+        else if(choix == 5){
+            resultat = KelvinVersFarenheit(var1);
+            avant = "Kelvin";
+            apres = "Farenheit";
+        }
+        else if(choix == 6){
+            resultat = FarenheitVersKelvin(var1);
+            avant = "Farenheit";
+            apres = "Kelvin";
+        }
+        else{
+            System.out.println("erreur");
+        }
+        System.out.println(var1 + " degré " + avant + " est égal à " + resultat + " degré " + apres);
+               
         
     }
     
