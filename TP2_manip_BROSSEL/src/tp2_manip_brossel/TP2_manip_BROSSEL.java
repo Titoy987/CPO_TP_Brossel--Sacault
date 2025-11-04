9f91b43d513dfbd8b06b9fd52da3eb3b68ce88f0
steven <steven@10.3.61.29>
2025-11-04 10:39:39 +0100

    TP2_manip_termminer

--- TP2_manip_BROSSEL/src/tp2_manip_brossel/TP2_manip_BROSSEL.java
+++ TP2_manip_BROSSEL/src/tp2_manip_brossel/TP2_manip_BROSSEL.java
@@ -14,7 +14,20 @@ public class TP2_manip_BROSSEL {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Moussaka[] tab=new Moussaka[10];
+        
+        Tartiflette assiette1 = new Tartiflette(500) ;
+        Tartiflette assiette2 = new Tartiflette(600) ;
+        Tartiflette assiette3 = assiette2 ;
+        assiette1=assiette2;
+        assiette2=assiette3;
+        System.out.println("nb de calories de Assiette 2 : " +assiette2.nbCalories) ;
+        System.out.println("nb de calories de Assiette 3 : " +assiette3.nbCalories) ;
+        
+        for(int i=0; i<tab.length;i++){
+            Moussaka var= new Moussaka(i*100);
+            tab[i]=var;
+        }
     }
     
 }
