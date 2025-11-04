e135ab7e2a745aab472d8f63cd3ca418b84055fd
steven <steven@10.3.61.29>
2025-11-04 11:16:15 +0100

    Q9 fini

--- TP2_relation_1_BROSSEL/src/tp2_relation_1_brossel/TP2_relation_1_BROSSEL.java
+++ TP2_relation_1_BROSSEL/src/tp2_relation_1_brossel/TP2_relation_1_BROSSEL.java
@@ -18,9 +18,29 @@ public class TP2_relation_1_BROSSEL {
         Voiture uneAutreClio = new Voiture ("Clio", "Renault", 5 ) ;
         Voiture une2008 = new Voiture ("2008", "Peugeot", 6 ) ;
         Voiture uneMicra = new Voiture ("Micra", "Nissan", 4 ) ;
+        
         Personne bob = new Personne("Bobby", "Sixkiller");
         Personne reno = new Personne("Reno", "Raines");
+        
         System.out.println("liste des voitures disponibles "+ uneClio +"\n" + uneAutreClio + "\n" + une2008 + "\n" + uneMicra ) ;
+        
+        bob.liste_voitures[bob.nbVoitures] = uneClio ;
+        uneClio.Proprietaire = bob;
+        bob.nbVoitures++;
+        
+        bob.liste_voitures[bob.nbVoitures] = uneAutreClio ;
+        uneAutreClio.Proprietaire = bob;
+        bob.nbVoitures++;
+        
+        reno.liste_voitures[reno.nbVoitures]=une2008;
+        une2008.Proprietaire =reno;
+        reno.nbVoitures++;
+        
+        reno.liste_voitures[reno.nbVoitures]=uneMicra;
+        uneMicra.Proprietaire =reno;
+        reno.nbVoitures++;
+        
+        System.out.println("la premiere voiture de Bob est " +bob.liste_voitures[0] ) ;
     }
     
 }
