38a88216a3fd11b46c96992fb441ba8828a7e110
steven <steven@10.3.61.29>
2025-11-04 11:16:23 +0100

    Q9 fini

---
+++ TP2_relation_1_BROSSEL/src/tp2_relation_1_brossel/Voiture.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_relation_1_brossel;
+
+/**
+ *
+ * @author steven
+ */
+public class Voiture {
+    String Modele;
+    String Marque;
+    int PuissanceCV;
+    Personne Proprietaire;
+    public Voiture(String unModele , String uneMarque, int CV){
+        Modele = unModele;
+        Marque = uneMarque;
+        PuissanceCV = CV;
+        Proprietaire = null;
+    }
+     @Override
+    public String toString() {
+        return Modele+Marque+PuissanceCV;
+    }
+}
