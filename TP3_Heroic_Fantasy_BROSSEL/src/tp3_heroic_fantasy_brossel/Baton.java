d2adb29b6236f41b7660f48659c07daae0b91fd9
steven <steven@10.3.61.29>
2025-11-19 10:14:43 +0100

    commit

---
+++ TP3_Heroic_Fantasy_BROSSEL/src/tp3_heroic_fantasy_brossel/Baton.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp3_heroic_fantasy_brossel;
+
+/**
+ *
+ * @author steven
+ */
+public class Baton extends Arme {
+
+    private int age; // < 100
+
+    public Baton(String nom, int niveauAttaque, int age) {
+        super(nom, niveauAttaque); // constructeur de la classe Arme
+        if (age > 100) {
+            this.age = 100;
+        } else if (age < 0) {
+            this.age = 0;
+        } else {
+            this.age = age;
+        }
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Âge : " + age;
+    }
+}
