7bf74c86c664890081f0a50fc8cd979316de114a
steven <steven@10.3.61.29>
2025-11-19 10:18:49 +0100

    commit

--- TP3_Heroic_Fantasy_BROSSEL/src/tp3_heroic_fantasy_brossel/TP3_Heroic_Fantasy_BROSSEL.java
+++ TP3_Heroic_Fantasy_BROSSEL/src/tp3_heroic_fantasy_brossel/TP3_Heroic_Fantasy_BROSSEL.java
@@ -4,6 +4,11 @@
  */
 package tp3_heroic_fantasy_brossel;
 
+import Armes.Baton;
+import Armes.Arme;
+import Armes.Epee;
+import java.util.Vector;
+
 /**
  *
  * @author steven
@@ -14,7 +19,21 @@ public class TP3_Heroic_Fantasy_BROSSEL {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Epee epee1 = new Epee("Excalibur", 7, 5);
+        Epee epee2 = new Epee("Durandal", 4, 7);
+
+        Baton baton1 = new Baton("Chêne", 4, 5);
+        Baton baton2 = new Baton("Charme", 5, 6);
+
+        Vector<Arme> armes = new Vector<>();
+        armes.add(epee1);
+        armes.add(epee2);
+        armes.add(baton1);
+        armes.add(baton2);
+
+        for (int i = 0; i < armes.size(); i++) {
+            System.out.println(armes.elementAt(i).toString());
+        }
     }
     
 }
