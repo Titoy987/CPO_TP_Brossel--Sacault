70c560965d231157c190b9a802494fc1b63ebea3
steven <steven@10.3.61.29>
2025-12-15 16:33:57 +0100

    commit

---
+++ LightOff_BROSSEL_version_console/src/lightoff_brossel_version_console/LightOff_BROSSEL_version_console.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lightoff_brossel_version_console;
+
+/**
+ *
+ * @author steven
+ */
+public class LightOff_BROSSEL_version_console {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
